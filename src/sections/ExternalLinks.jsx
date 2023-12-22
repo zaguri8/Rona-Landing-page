603c9fa9b3efef1bb630d8e6cab9d2c41ba01caf
Nadav <33123586+zaguri8@users.noreply.github.com>
2023-12-22 13:11:18 +0200

    phone number

--- src/sections/ExternalLinks.jsx
+++ src/sections/ExternalLinks.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const Links = [
     { link: "https://www.tiktok.com/@ronaazarya_midwife?_t=8htoZNvbFlR&_r=1", icon: tiktok },
-    { link: "https://wa.link/e19ca6", icon: whatsapp },
+    { link: "https://wa.link/fzb672", icon: whatsapp },
     { link: "https://instagram.com/ronaazarya_midwife?igshid=NGVhN2U2NjQ0Yg%3D%3D&utm_source=qr", icon: instagram },
 ]
 export default function ExternalLinks() {
